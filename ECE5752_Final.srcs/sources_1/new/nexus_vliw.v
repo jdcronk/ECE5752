4b3f47cc9cd2c2636ef940705fedf406a9fa582b
Joshua Cronk <jdcronk@umich.edu>
2015-03-18 23:03:37 -0400

    Added Top Design
    * Added the top module for the CPU

---
+++ ECE5752_Final.srcs/sources_1/new/nexus_vliw.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/18/2015 08:48:25 PM
+// Design Name: 
+// Module Name: nexus_vliw
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module nexus_vliw( //Inputs
+		   clock,
+		   reset
+		  );
+   // Inputs
+   input clock;  // System Clock
+   input reset;  // System Reset
+   
+endmodule
