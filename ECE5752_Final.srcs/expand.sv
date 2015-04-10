57c5876c4981e61d0ed32a80b27720c962335f28
Joshua Cronk <jdcronk@umich.edu>
2015-04-10 00:38:55 -0400

    Expand Stage
    * Added some basic inputs and outputs to the expand stage

--- ECE5752_Final.srcs/expand.sv
+++ ECE5752_Final.srcs/expand.sv
@@ -1,5 +1,18 @@
 `timescale 1ns / 100ps
 
-module expand (
-);
+module expand(//Inputs
+              clock,
+              reset,
+              inst_bundle,
+              //Outputs
+              expanded_insts
+              );
+
+    //Inputs
+    input        clock;
+    input        reset;
+    input [31:0] inst_bundle [5:0]; // The six incoming instructions
+            
+    //Outputs
+    output [31:0] expanded_insts [5:0]; // The six expanded instructions
 endmodule
