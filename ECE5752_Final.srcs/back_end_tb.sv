5f45987e2056ef1682742859fa384727fb9a95c1
Joshua Cronk <jdcronk@umich.edu>
2015-04-19 15:03:18 -0400

    Updated Back End Test Bench
    * The back end test bench has been updated to better test the back end
    of the chip

--- ECE5752_Final.srcs/back_end_tb.sv
+++ ECE5752_Final.srcs/back_end_tb.sv
@@ -1,17 +1,27 @@
 `timescale 1ns / 100ps
 
+`define ADD_R0_R1_R2_1 41'b10000001_000000_0000010_0000001_0000000_000000
+`define ADD_R3_R3_R3_1 41'b10000001_000000_0000011_0000011_0000011_000000
+`define ADD_R0_R0_R0   41'b10000000_000000_0000000_0000000_0000000_000000
+
 module back_end_tb;
 
     reg         clock;  // System Clock
     reg         reset;  // System Reset
-    reg [127:0] inst_bundle [1:0]; // The six incoming instructions in their bundles
-    reg         valid       [1:0];
+    reg  [31:0] clock_count;
+    reg [127:0] inst_bundle  [1:0]; // The six incoming instructions in their bundles
+    reg         valid        [1:0];
+    
+    wire        stall_buffer [1:0];
+    wire [63:0] cdb_data     [3:0];
 
     back_end be0(
 	       	     .clock(clock),
 		         .reset(reset),
 		         .inst_bundle(inst_bundle),
-		         .valid(valid)
+		         .valid(valid),
+		         .stall_buffer(stall_buffer),
+		         .cdb_data(cdb_data)
 		         );
     // Generate System Clock
     always
@@ -30,10 +40,13 @@ module back_end_tb;
         @(posedge clock);
         reset = 1'b0; 
         @(posedge clock);
+        @(posedge clock);
+        /*
         `SD;
         inst_bundle[0] = {`NOOP_INST,`NOOP_INST,`NOOP_INST,5'b00000};
         @(posedge clock);
-        inst_bundle[0][86:46] = 41'b10000001000000000010000010000000001000000;
+        inst_bundle[0][86:46] = `ADD_R0_R1_R2_1;
+        inst_bundle[0][127:87] = `ADD_R3_R3_R3_1;
         valid[0]             = 1'b1;
         @(posedge clock);
         valid[0]             = 1'b0;
@@ -44,9 +57,43 @@ module back_end_tb;
         @(posedge clock);
         valid[0]             = 1'b0;
         @(posedge clock);
-        inst_bundle[0][86:46] = 41'b10000001000000000010000010000000001000000;
+        inst_bundle[0][86:46] = `ADD_R0_R0_R0;
         valid[0]             = 1'b1;
         @(posedge clock);
         valid[0]             = 1'b0;
-    end		         
+        */
+    end		
+    
+    always @(posedge clock)
+    begin
+        if (reset) begin
+            inst_bundle[0] <= `SD {`NOOP_INST,`NOOP_INST,`NOOP_INST,5'b00000};
+            inst_bundle[1] <= `SD {`NOOP_INST,`NOOP_INST,`NOOP_INST,5'b00000};
+            valid[0]       <= `SD 0;
+            valid[1]       <= `SD 0; 
+            clock_count    <= `SD 0;
+        end
+        else begin
+            if (clock_count == 0) begin
+                inst_bundle[0][86:46]  <= `SD `ADD_R0_R1_R2_1;
+                inst_bundle[0][127:87] <= `SD `ADD_R3_R3_R3_1;
+                inst_bundle[1]         <= `SD {`NOOP_INST,`NOOP_INST,`NOOP_INST,5'b00000};
+                valid[0]               <= `SD 1;
+                valid[1]               <= `SD 0;
+            end
+            else if (clock_count == 1) begin
+                valid[0] <= `SD 0;
+                valid[1] <= `SD 0;
+            end
+            else begin
+                inst_bundle[0] <= `SD {`NOOP_INST,`NOOP_INST,`NOOP_INST,5'b00000};
+                inst_bundle[1] <= `SD {`NOOP_INST,`NOOP_INST,`NOOP_INST,5'b00000};
+                valid[0]       <= `SD 0;
+                valid[1]       <= `SD 0; 
+            end
+            
+            clock_count <= `SD (clock_count + 1);
+            reset       <= `SD 0;
+        end
+    end         
 endmodule
